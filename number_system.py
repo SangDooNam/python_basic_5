4bd5dddb26a239473e10b1eb5fece68cf484ab5a
SangDoo Nam <kexe009@hotmail.com>
2023-08-22 16:26:51 +0200

    add number_system.py

---
+++ number_system.py
@@ -0,0 +1,79 @@
+#Task 1 : Write a program that converts an integer in decimal format to binary format.
+
+# print(bin(12491312452))
+
+num = 12491312452
+
+binary = bin(num)
+
+print('1 :', binary)
+
+
+#Task 2 : Convert a binary number to its decimal format.
+
+# print(int(0b1010100010010100100100010100100))
+
+num = 0b1010100010010100100100010100100
+
+integer = int(num)
+
+print('2 :', integer)
+
+
+#Task 3 : Convert a decimal number to its hexadecimal format.
+
+# print(hex(29384792380519283098))
+
+num = 29384792380519283098
+
+hexadecimal = hex(num)
+
+print('3 :', hexadecimal )
+
+#Task 4 : Convert a hexadecimal number to its decimal format.
+
+# print(int(0x123abcd45890f))
+num = 0x1243faeccccc
+decimal = int(num)
+
+print('4 :', decimal)
+
+#Task 5 : Convert a binary number to its hexadecimal format.
+
+num = 0b010001010101011101010101
+
+binary = bin(num)
+
+print('5 :', binary)
+
+# print(hex(0b010001010101011101010101))
+
+#Task 6 : Convert a hexadecimal number to its binary format.
+
+num = 0x123ffffff35450400ff
+
+binary = bin(num)
+
+print('6 :', binary)
+
+# print(bin(0x123ffffff35450400ff))
+
+#Task 7 : Convert a decimal number to its octal format.
+
+# print(oct(43235234))
+
+num = 43235234
+
+octal = oct(num)
+
+print('7 :', octal)
+
+#Task 8 : Convert a octal number to its decimal format.
+
+# print(int(0o123567123572346527))
+
+num = 0o123567123572346527
+
+decimal = int(num)
+
+print('8 :', decimal)
